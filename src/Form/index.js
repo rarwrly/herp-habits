fe27097b0ffe0c3aa0b21fda26c8999f186b2668
rarwrly <ehc2642@gmail.com>
2020-12-10 18:02:22 -0500

    2nd attempt at the separate form

--- src/Form/index.js
+++ src/Form/index.js
@@ -1,21 +1,55 @@
-const Form = () => {
-    const [selectedOption, setSelectedOption] = useState(1);  
-    
-    return (
-      <div>
-        <p>selected option: {selectedOption}</p>
-        <form>
-          <select
-            onChange={(event) => setSelectedOption(event.target.value)}
-            value={selectedOption}
-            defaultValue={selectedOption}
-          >
-            <option value="1">1</option>
-            <option value="2">2</option>
-            <option value="3">3</option>
-            <option value="4">4</option>
-          </select>
-        </form>
-      </div>
-    );
+import React, { useState, useEffect } from "react";
+import {useForm} from "react-hook-form";
+
+const Form = (props) => {
+  
+  const [selectedOption, setSelectedOption] = useState("no food offered"); 
+  const { register, handleSubmit, reset, errors} =useForm(); 
+  const onSubmit = async (formData) =>{
+      const response = await fetch(
+          `http://localhost:3000/reptiles/${props.reptileId}/logs`,
+          {
+              method: 'POST', 
+              body: JSON.stringify(formData),
+
+              headers:{
+              "Content-Type":"application/json",
+              },
+          }
+      );
+      
+      console.log(formData);
+      const createdLog= await response.json();
+      reset();  
+      props.setEntries([createdLog, ...props.entries]);
   };
+
+    return (
+      <form onSubmit={handleSubmit(onSubmit)}>
+      <ul class="formWrapper">
+           <li class="formItem"><lable>Date</lable>
+               <input name="date" type="date" ref={register({ required:"You must enter a date"})}/>
+               {errors.date && errors.date.message}
+           </li>
+           <li class="formItem"><lable>Comments</lable>
+               <input name="comments" ref={register({ required:"You must enter a comment"})}/>
+               {errors.comments && errors.comments.message}
+           </li>
+           <li class="formItem"><lable>Prey Items:</lable>
+               <select name="preyItems"ref={register({required: "You must select an option"})}
+               defaultValue={"no food offered"}>
+                   <option value="1 rat">1 rat </option>
+                   <option value="1 rat, 1 mouse">1 rat, 1 mouse</option>
+                   <option value="2 mice">2 mice</option>
+                   <option value="refused prey item">refused prey item</option>
+                   <option value="no food offered">no food offered</option>
+               </select>
+           </li>
+           <li class="formItem"><input type="submit" value="Submit Log Entry"/>
+           </li>
+      </ul>  
+   </form>
+    )
+  }
+
+  export default Form;
